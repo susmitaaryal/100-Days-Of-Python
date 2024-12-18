c0d82e33fbf2f55da661aa9bdf7a38834a0025dd
susmitaaryal <85101132+susmitaaryal@users.noreply.github.com>
2024-12-18 20:11:39 +0545

    Add files via upload

---
+++ day5/adding_even_number.py
@@ -0,0 +1,4 @@
+total_sum = 0
+for number in range (2,101,2):
+    total_sum = total_sum + number
+print(total_sum)
---
+++ day5/fizz_buzz.py
@@ -0,0 +1,12 @@
+
+
+for number in range (1,101):
+    if number % 3 == 0 and number % 5 ==0:
+        print("fizzBuzz")
+    elif number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("buzz")
+    else:
+        print(number)
+    
